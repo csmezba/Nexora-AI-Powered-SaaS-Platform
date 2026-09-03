3bc59ecabec70f25ee09f8ec0ffed0e86707480d
Nokibul Mezba <marmotalpine@gmail.com>
2026-09-03 20:13:30 +0600

    feat: enable CORS in application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { GlobalExceptionFilter } from './common/filters/global-exception.filter.
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
